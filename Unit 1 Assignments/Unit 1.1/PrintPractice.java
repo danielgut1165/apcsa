ba673116eddceb147f9a3450971da20152f6a2f3
danielgut1165 <daniel.g.2760@gmail.com>
2024-12-30 00:24:57 -0600

    Initial commit

---
+++ Unit 1 Assignments/Unit 1.1/PrintPractice.java
@@ -0,0 +1,34 @@
+public class PrintPractice {
+    public static void main(String[] args) {
+        // For the follow code, I would like for you to print out the text specified
+        // You many only use ONE println statement, you don't need print statments for this assignment
+        // <TAB> means the tab key on your keyboard. Look up how to print a tab character in java
+        // <ENTER> means the enter key on your keybaord. Look up how to print a new line character in java
+        // Basically, I want YOU to do some research on 'escape sequences' in java 
+        // Here is an example:
+
+        // Hello World
+        System.out.println("Hello World");
+
+        // Mary had a little lamb
+
+
+        // Mary<TAB>had a little<TAB>lamb
+
+        
+        // Mary had a<ENTER>little lamb
+
+        
+        // 'Mary had a little lamb'
+
+        
+        // "Mary had a little lamb"
+
+        
+        // Mary\had<ENTER><ENTER><TAB>a little\lamb
+
+        
+        // "\t"
+        
+    }
+}
