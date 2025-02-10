3834d528dc849e02783bb5db98c7434dd98a47f5
danielgut12 <daniel.g.2760@gmail.com>
2025-02-10 13:10:28 -0600

    Comments added

--- Unit 2 Assignments/Unit 2.1/MazePractice.java
+++ Unit 2 Assignments/Unit 2.1/MazePractice.java
@@ -1,7 +1,10 @@
 public class MazePractice {
     public static void main(String[] args) {
+        // Create the maze
+        
+        // Move your character to the end
 
-        // Uncomment this line for debugging purposes
+        // Uncomment this line for debugging purposes, you may need to change the maze varaible name to yours
         // maze.print();
     }
 }
